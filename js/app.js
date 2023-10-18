485ee8df7131d166dc5161d46bf7def8315b1322
AyanRaissov <100411660+AyanRaissov@users.noreply.github.com>
2023-10-18 17:05:30 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,155 @@
+
+$(function() {
+
+
+    /* Fixed Header */
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+    checkScroll(scrollPos, introH);
+
+    $(window).on("scroll resize", function() {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        checkScroll(scrollPos, introH);
+    });
+
+    function checkScroll(scrollPos, introH) {
+        if( scrollPos > introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+
+
+    /* Smooth scroll */
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass("show");
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 70
+        }, 700);
+    });
+
+
+
+    /* Nav Toggle */
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    });
+
+    
+
+    /* Reviews: https://kenwheeler.github.io/slick/ */
+    let slider = $("#reviewsSlider");
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+    });
+
+    
+
+
+});
+
+// -------------------------------------
+AOS.init();
+
+
+// /---------------------------------/
+
+const btn_all_filials = document.querySelector('.btn_all_filials')
+const sectFilial = document.querySelector('.reviews .container section')
+const filial = document.querySelector('.filial')
+
+let sectFilialDefHeight = 350
+
+let countClick = 0
+
+
+btn_all_filials.addEventListener('click', ()=>{
+    if(countClick % 2 == 0){
+		const filialStyles = getComputedStyle(filial)
+		const filialHeight = filialStyles.height
+		sectFilial.style.height = filialHeight
+	}
+	else{
+		sectFilial.style.height =  sectFilialDefHeight + 'px'
+	}
+	countClick++
+})
+
+// --------------------------------------
+
+document.addEventListener('mousemove', e => {
+	Object.assign(document.documentElement, {
+		style: `
+		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+		--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
+		`
+	})
+})
+ 
+ 
+
+ymaps.ready(function () {
+    var myMap = new ymaps.Map("YMapsID", {
+      center: [51.20788295267876,71.35406355753322],
+      zoom: 16
+    });
+     
+  var myPlacemark = new ymaps.GeoObject({
+    geometry: {
+        type: "Point",
+        coordinates: [51.20788295267876,71.35406355753322]
+    }    
+    
+});
+myMap.geoObjects.add(myPlacemark);
+});
+
+
+
+//   const openModal = document.querySelectorAll('.open_modal')
+// const clsModal = document.querySelector('.cls_modal')
+// const modalWindow = document.querySelector('.modal_window')
+
+// openModal.forEach((item)=>{
+//     item.onclick = ()=>{
+// 	modalWindow.style.display = 'flex'
+// 	setTimeout(()=>{
+// 		modalWindow.style.opacity = 1
+// 	}, 300)
+// }})
+
+// clsModal.addEventListener('click', ()=>{
+// 	modalWindow.style.opacity = 0
+// 	setTimeout(()=>{
+// 		modalWindow.style.display = 'none'
+// 	}, 300)
+// })
+
+// window.onclick = function(event) {
+//     if (event.target == modalWindow) {
+//         modalWindow.style.display = "none";
+//     }
+//   }
---
+++ js/modal.js
@@ -0,0 +1,367 @@
+const openModal_01 = document.querySelector('.open_modal_01')
+const clsModal_01 = document.querySelector('.cls_modal_01')
+const modalWindow_01 = document.querySelector('.modal_window_01')
+
+openModal_01.addEventListener('click', ()=>{
+	modalWindow_01.style.display = 'flex'
+	setTimeout(()=>{
+		modalWindow_01.style.opacity = 1
+	}, 300)
+    window.onclick = function(event) {
+        if (event.target == modalWindow_01) {
+            modalWindow_01.style.display = "none";
+        }
+      }
+})
+
+clsModal_01.addEventListener('click', ()=>{
+	modalWindow_01.style.opacity = 0
+	setTimeout(()=>{
+		modalWindow_01.style.display = 'none'
+	}, 300)
+})
+
+
+
+  const openModal_02 = document.querySelector('.open_modal_02')
+  const clsModal_02 = document.querySelector('.cls_modal_02')
+  const modalWindow_02 = document.querySelector('.modal_window_02')
+  
+  openModal_02.addEventListener('click', ()=>{
+	modalWindow_02.style.display = 'flex'
+	setTimeout(()=>{
+		modalWindow_02.style.opacity = 1
+	}, 300)
+    window.onclick = function(event) {
+        if (event.target == modalWindow_02) {
+            modalWindow_02.style.display = "none";
+        }
+      }
+})
+  
+  clsModal_02.addEventListener('click', ()=>{
+      modalWindow_02.style.opacity = 0
+      setTimeout(()=>{
+          modalWindow_02.style.display = 'none'
+      }, 300)
+  })
+  
+  
+
+
+    const openModal_03 = document.querySelector('.open_modal_03')
+    const clsModal_03 = document.querySelector('.cls_modal_03')
+    const modalWindow_03 = document.querySelector('.modal_window_03')
+    
+    openModal_03.addEventListener('click', ()=>{
+      modalWindow_03.style.display = 'flex'
+      setTimeout(()=>{
+          modalWindow_03.style.opacity = 1
+      }, 300)
+      window.onclick = function(event) {
+        if (event.target == modalWindow_03) {
+            modalWindow_03.style.display = "none";
+        }
+      }
+  })
+    
+    clsModal_03.addEventListener('click', ()=>{
+        modalWindow_03.style.opacity = 0
+        setTimeout(()=>{
+            modalWindow_03.style.display = 'none'
+        }, 300)
+    })
+
+
+
+    const openModal_04 = document.querySelector('.open_modal_04')
+    const clsModal_04 = document.querySelector('.cls_modal_04')
+    const modalWindow_04 = document.querySelector('.modal_window_04')
+    
+    openModal_04.addEventListener('click', ()=>{
+      modalWindow_04.style.display = 'flex'
+      setTimeout(()=>{
+          modalWindow_04.style.opacity = 1
+      }, 300)
+      window.onclick = function(event) {
+        if (event.target == modalWindow_04) {
+            modalWindow_04.style.display = "none";
+        }
+      }
+  })
+    
+    clsModal_04.addEventListener('click', ()=>{
+        modalWindow_04.style.opacity = 0
+        setTimeout(()=>{
+            modalWindow_04.style.display = 'none'
+        }, 300)
+    })
+
+
+
+    const openModal_05 = document.querySelector('.open_modal_05')
+    const clsModal_05 = document.querySelector('.cls_modal_05')
+    const modalWindow_05 = document.querySelector('.modal_window_05')
+    
+    openModal_05.addEventListener('click', ()=>{
+      modalWindow_05.style.display = 'flex'
+      setTimeout(()=>{
+          modalWindow_05.style.opacity = 1
+      }, 300)
+      window.onclick = function(event) {
+        if (event.target == modalWindow_05) {
+            modalWindow_05.style.display = "none";
+        }
+      }
+  })
+    
+    clsModal_05.addEventListener('click', ()=>{
+        modalWindow_05.style.opacity = 0
+        setTimeout(()=>{
+            modalWindow_05.style.display = 'none'
+        }, 300)
+    })
+
+
+
+    const openModal_06 = document.querySelector('.open_modal_06')
+    const clsModal_06 = document.querySelector('.cls_modal_06')
+    const modalWindow_06 = document.querySelector('.modal_window_06')
+    
+    openModal_06.addEventListener('click', ()=>{
+      modalWindow_06.style.display = 'flex'
+      setTimeout(()=>{
+          modalWindow_06.style.opacity = 1
+      }, 300)
+      window.onclick = function(event) {
+        if (event.target == modalWindow_06) {
+            modalWindow_06.style.display = "none";
+        }
+      }
+  })
+    
+    clsModal_06.addEventListener('click', ()=>{
+        modalWindow_06.style.opacity = 0
+        setTimeout(()=>{
+            modalWindow_06.style.display = 'none'
+        }, 300)
+    })
+
+
+
+    const openModal_07 = document.querySelector('.open_modal_07')
+    const clsModal_07 = document.querySelector('.cls_modal_07')
+    const modalWindow_07 = document.querySelector('.modal_window_07')
+    
+    openModal_07.addEventListener('click', ()=>{
+      modalWindow_07.style.display = 'flex'
+      setTimeout(()=>{
+          modalWindow_07.style.opacity = 1
+      }, 300)
+      window.onclick = function(event) {
+        if (event.target == modalWindow_07) {
+            modalWindow_07.style.display = "none";
+        }
+      }
+  })
+    
+    clsModal_07.addEventListener('click', ()=>{
+        modalWindow_07.style.opacity = 0
+        setTimeout(()=>{
+            modalWindow_07.style.display = 'none'
+        }, 300)
+    })
+
+
+
+    const openModal_08 = document.querySelector('.open_modal_08')
+    const clsModal_08 = document.querySelector('.cls_modal_08')
+    const modalWindow_08 = document.querySelector('.modal_window_08')
+    
+    openModal_08.addEventListener('click', ()=>{
+      modalWindow_08.style.display = 'flex'
+      setTimeout(()=>{
+          modalWindow_08.style.opacity = 1
+      }, 300)
+      window.onclick = function(event) {
+        if (event.target == modalWindow_08) {
+            modalWindow_08.style.display = "none";
+        }
+      }
+  })
+    
+    clsModal_08.addEventListener('click', ()=>{
+        modalWindow_08.style.opacity = 0
+        setTimeout(()=>{
+            modalWindow_08.style.display = 'none'
+        }, 300)
+    })
+
+
+
+    const openModal_09 = document.querySelector('.open_modal_09')
+    const clsModal_09 = document.querySelector('.cls_modal_09')
+    const modalWindow_09 = document.querySelector('.modal_window_09')
+    
+    openModal_09.addEventListener('click', ()=>{
+      modalWindow_09.style.display = 'flex'
+      setTimeout(()=>{
+          modalWindow_09.style.opacity = 1
+      }, 300)
+      window.onclick = function(event) {
+        if (event.target == modalWindow_09) {
+            modalWindow_09.style.display = "none";
+        }
+      }
+  })
+    
+    clsModal_09.addEventListener('click', ()=>{
+        modalWindow_09.style.opacity = 0
+        setTimeout(()=>{
+            modalWindow_09.style.display = 'none'
+        }, 300)
+    })
+
+
+
+    const openModal_10 = document.querySelector('.open_modal_10')
+    const clsModal_10 = document.querySelector('.cls_modal_10')
+    const modalWindow_10 = document.querySelector('.modal_window_10')
+    
+    openModal_10.addEventListener('click', ()=>{
+      modalWindow_10.style.display = 'flex'
+      setTimeout(()=>{
+          modalWindow_10.style.opacity = 1
+      }, 300)
+      window.onclick = function(event) {
+        if (event.target == modalWindow_10) {
+            modalWindow_10.style.display = "none";
+        }
+      }
+  })
+    
+    clsModal_10.addEventListener('click', ()=>{
+        modalWindow_10.style.opacity = 0
+        setTimeout(()=>{
+            modalWindow_10.style.display = 'none'
+        }, 300)
+    })
+
+
+    const openModal_11 = document.querySelector('.open_modal_11')
+    const clsModal_11 = document.querySelector('.cls_modal_11')
+    const modalWindow_11 = document.querySelector('.modal_window_11')
+    
+    openModal_11.addEventListener('click', ()=>{
+      modalWindow_11.style.display = 'flex'
+      setTimeout(()=>{
+          modalWindow_11.style.opacity = 1
+      }, 300)
+      window.onclick = function(event) {
+        if (event.target == modalWindow_11) {
+            modalWindow_11.style.display = "none";
+        }
+      }
+  })
+    
+    clsModal_11.addEventListener('click', ()=>{
+        modalWindow_11.style.opacity = 0
+        setTimeout(()=>{
+            modalWindow_11.style.display = 'none'
+        }, 300)
+    })
+
+
+    const openModal_12 = document.querySelector('.open_modal_12')
+    const clsModal_12 = document.querySelector('.cls_modal_12')
+    const modalWindow_12 = document.querySelector('.modal_window_12')
+    
+    openModal_12.addEventListener('click', ()=>{
+      modalWindow_12.style.display = 'flex'
+      setTimeout(()=>{
+          modalWindow_12.style.opacity = 1
+      }, 300)
+      window.onclick = function(event) {
+        if (event.target == modalWindow_12) {
+            modalWindow_12.style.display = "none";
+        }
+      }
+  })
+    
+    clsModal_12.addEventListener('click', ()=>{
+        modalWindow_12.style.opacity = 0
+        setTimeout(()=>{
+            modalWindow_12.style.display = 'none'
+        }, 300)
+    })
+
+    const openModal_13 = document.querySelector('.open_modal_13')
+    const clsModal_13 = document.querySelector('.cls_modal_13')
+    const modalWindow_13 = document.querySelector('.modal_window_13')
+    
+    openModal_13.addEventListener('click', ()=>{
+      modalWindow_13.style.display = 'flex'
+      setTimeout(()=>{
+          modalWindow_13.style.opacity = 1
+      }, 300)
+      window.onclick = function(event) {
+        if (event.target == modalWindow_13) {
+            modalWindow_13.style.display = "none";
+        }
+      }
+  })
+    
+    clsModal_13.addEventListener('click', ()=>{
+        modalWindow_13.style.opacity = 0
+        setTimeout(()=>{
+            modalWindow_13.style.display = 'none'
+        }, 300)
+    })
+
+    const openModal_14 = document.querySelector('.open_modal_14')
+    const clsModal_14 = document.querySelector('.cls_modal_14')
+    const modalWindow_14 = document.querySelector('.modal_window_14')
+    
+    openModal_14.addEventListener('click', ()=>{
+      modalWindow_14.style.display = 'flex'
+      setTimeout(()=>{
+          modalWindow_14.style.opacity = 1
+      }, 300)
+      window.onclick = function(event) {
+        if (event.target == modalWindow_14) {
+            modalWindow_14.style.display = "none";
+        }
+      }
+  })
+    
+    clsModal_14.addEventListener('click', ()=>{
+        modalWindow_14.style.opacity = 0
+        setTimeout(()=>{
+            modalWindow_14.style.display = 'none'
+        }, 300)
+    })
+
+
+
+    const openModal_15 = document.querySelector('.open_modal_15')
+    const clsModal_15 = document.querySelector('.cls_modal_15')
+    const modalWindow_15 = document.querySelector('.modal_window_15')
+    
+    openModal_15.addEventListener('click', ()=>{
+      modalWindow_15.style.display = 'flex'
+      setTimeout(()=>{
+          modalWindow_15.style.opacity = 1
+      }, 300)
+      window.onclick = function(event) {
+        if (event.target == modalWindow_15) {
+            modalWindow_15.style.display = "none";
+        }
+      }
+  })
+    
+    clsModal_15.addEventListener('click', ()=>{
+        modalWindow_15.style.opacity = 0
+        setTimeout(()=>{
+            modalWindow_15.style.display = 'none'
+        }, 300)
+    })
